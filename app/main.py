7706d1ede7a60cd95e3f570ef0cfcc82e16cfb65
Soma Sundaram Sekar <somasundaram.sekar@Somas-MBP.fritz.box>
2024-08-31 11:28:34 +0200

    add glossary term

--- app/main.py
+++ app/main.py
@@ -2,10 +2,12 @@ from fastapi import FastAPI
 from elasticsearch import Elasticsearch
 from neo4j import GraphDatabase
 import os
-from app.api.endpoints import data_products
+from app.api.endpoints import data_products, glossary, search
 
 app = FastAPI()
 app.include_router(data_products.router, prefix="/api/v1")
+app.include_router(glossary.router, prefix="/api/v1")
+app.include_router(search.router, prefix="/api/v1")
 
 @app.get("/health")
 async def health_check():
